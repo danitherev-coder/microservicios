ca441a0ea5069bc0f1ac0edd47bb54f7929e7e0f
danitherev-coder <csaavedrac@alumno.unsm.edu.pe>
2024-03-13 09:52:12 -0500

    gateway

--- usuario-service/src/main/java/com/usuario/service/feignclient/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclient/CarroFeignClient.java
@@ -1,5 +1,6 @@
 package com.usuario.service.feignclient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +14,10 @@ import java.util.List;
 public interface CarroFeignClient {
     
     @PostMapping
+    @LoadBalanced
     public Carro save(@RequestBody Carro carro);
 
     @GetMapping("/usuario/{usuarioId}")
+    @LoadBalanced
     public List<Carro> getCarros(@PathVariable Long usuarioId);
 }
